741b03e5022878c9d303bd519009e9e25091f3d9
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2021-01-19 00:37:41 +0900

    SoulEaterAnimationを追加

--- sotukenGame/Sample_15_05/Sample_15_05/SoulEaterAnimation.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/SoulEaterAnimation.cpp
@@ -13,6 +13,48 @@ SoulEaterAnimation::~SoulEaterAnimation()
 
 bool SoulEaterAnimation::Start()
 {
+	//ƒAƒjƒ[ƒVƒ‡ƒ“ƒ[ƒhB
+	//‘Ò‹@
+	animClip[enIdle].Load("Assets/animData/DragonSoulEater/SoEa_idle.tka");
+	animClip[enIdle].SetLoopFlag(true);
+
+	//•à‚«
+	animClip[enWalk].Load("Assets/animData/DragonSoulEater/SoEa_walk.tka");
+	animClip[enWalk].SetLoopFlag(true);
+
+	//‘–‚è
+	animClip[enRun].Load("Assets/animData/DragonSoulEater/SoEa_run.tka");
+	animClip[enRun].SetLoopFlag(true);
+
+	//•‚—V
+	animClip[enFlyFloat].Load("Assets/animData/DragonSoulEater/SoEa_flyfloat.tka");
+	animClip[enFlyFloat].SetLoopFlag(true);
+	//”òs‘Oi
+	animClip[enFlyForward].Load("Assets/animData/DragonSoulEater/SoEa_flyforward.tka");
+	animClip[enFlyForward].SetLoopFlag(true);
+	//‘Î‹ó
+	animClip[enFlyGlide].Load("Assets/animData/DragonSoulEater/SoEa_flyglide.tka");
+	animClip[enFlyGlide].SetLoopFlag(true);
+	//™ôšK
+	animClip[enScream].Load("Assets/animData/DragonSoulEater/SoEa_scream.tka");
+	//UŒ‚
+	animClip[enBasicAttack].Load("Assets/animData/DragonSoulEater/SoEa_basic_ attack.tka");
+	//K”öUŒ‚
+	animClip[enTailAttack].Load("Assets/animData/DragonSoulEater/SoEa_tail_attack.tka");
+	//‰Î‰ŠUŒ‚
+	animClip[enFireballShoot].Load("Assets/animData/DragonSoulEater/SoEa_fireball shoot.tka");
+	//’…—¤
+	animClip[enFlyLand].Load("Assets/animData/DragonSoulEater/SoEa_flyland.tka");
+	//—£—¤
+	animClip[enTakeoff].Load("Assets/animData/DragonSoulEater/SoEa_takeoff.tka");
+	//–hŒä
+	animClip[enDefend].Load("Assets/animData/DragonSoulEater/SoEa_defend.tka");
+	//‡–°
+	animClip[enDefend].Load("Assets/animData/DragonSoulEater/SoEa_sleep.tka");
+	//”íƒ_ƒ
+	animClip[enGethit].Load("Assets/animData/DragonSoulEater/SoEa_gethit.tka");
+	//€–S
+	animClip[enDie].Load("Assets/animData/DragonSoulEater/SoEa_die.tka");
 	return true;
 }
 
--- sotukenGame/Sample_15_05/Sample_15_05/SoulEaterAnimation.h
+++ sotukenGame/Sample_15_05/Sample_15_05/SoulEaterAnimation.h
@@ -1,4 +1,23 @@
 #pragma once
+const enum SoulEaterAnim {
+	enIdle,                  //待機01
+	enWalk,                    //歩き
+	enRun,                     //走り
+	enBasicAttack,             //攻撃
+	enTailAttack,              //尻尾攻撃
+	enFireballShoot,           //火炎攻撃
+	enFlyFloat,                //浮遊
+	enFlyForward,              //飛行前進
+	enFlyGlide,                //対空
+	enFlyLand,                 //着陸
+	enTakeoff,                 //離陸
+	enDefend,                  //防御
+	enSleep,                   //睡眠
+	enScream,                  //咆哮
+	enGethit,                  //被ダメ
+	enDie,                     //死亡
+	enSoulEAnimClip_num        //アニメーションの数
+};
 class SoulEaterAnimation : public IGameObject
 {
 public:
@@ -19,6 +38,15 @@ public:
 	/// 更新関数。
 	/// </summary>
 	void Update() override;
+	/// <summary>
+	/// アニメーションクリップを返す。
+	/// </summary>
+	/// <returns></returns>
+	AnimationClip* GetAnimationClip()
+	{
+		return animClip;
+	}
 private:
+	AnimationClip animClip[enSoulEAnimClip_num];//アニメーションクリップ。
 };
 
