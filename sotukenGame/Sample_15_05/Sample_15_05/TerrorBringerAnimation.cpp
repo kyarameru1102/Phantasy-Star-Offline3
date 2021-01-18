7fa97e181424161cca216567c8fd3478dd4aec0c
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2021-01-19 08:59:06 +0900

    TerrorBringerAnimationを追加

---
+++ sotukenGame/Sample_15_05/Sample_15_05/TerrorBringerAnimation.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "TerrorBringerAnimation.h"
+
+TerrorBringerAnimation::TerrorBringerAnimation()
+{
+
+}
+
+TerrorBringerAnimation::~TerrorBringerAnimation()
+{
+
+}
---
+++ sotukenGame/Sample_15_05/Sample_15_05/TerrorBringerAnimation.h
@@ -0,0 +1,14 @@
+#pragma once
+class TerrorBringerAnimation
+{
+public:
+	/// <summary>
+	/// コンストラクタ。
+	/// </summary>
+	TerrorBringerAnimation();
+	/// <summary>
+	/// デストラクタ。
+	/// </summary>
+	~TerrorBringerAnimation();
+};
+
