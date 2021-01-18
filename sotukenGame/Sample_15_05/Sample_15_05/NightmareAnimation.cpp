d2656c39c139b54c7a671b3ca8c6b5994ce667a8
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2021-01-18 14:50:34 +0900

    NightmareAnimationを追加

--- sotukenGame/Sample_15_05/Sample_15_05/NightmareAnimation.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/NightmareAnimation.cpp
@@ -10,3 +10,51 @@ NightmareAnimation::~NightmareAnimation()
 {
 
 }
+
+bool NightmareAnimation::Start()
+{
+	//ƒAƒjƒ[ƒVƒ‡ƒ“ƒ[ƒhB
+	//‘Ò‹@‚O‚P
+	animClip[enIdle01].Load("Assets/animData/DragonNightmare/Nm_idle01.tka");
+	animClip[enIdle01].SetLoopFlag(true);
+	//‘Ò‹@‚O‚Q
+	animClip[enIdle02].Load("Assets/animData/DragonNightmare/Nm_idle02.tka");
+	animClip[enIdle02].SetLoopFlag(true);
+	//•à‚«
+	animClip[enWalk].Load("Assets/animData/DragonNightmare/Nm_walk.tka");
+	animClip[enWalk].SetLoopFlag(true);
+	//Œã‚ë•à‚«
+	animClip[enWalkBack].Load("Assets/animData/DragonNightmare/Nm_walkback.tka");
+	animClip[enWalkBack].SetLoopFlag(true);
+	//¶•à‚«
+	animClip[enWalkLeft].Load("Assets/animData/DragonNightmare/Nm_walkleft.tka");
+	animClip[enWalkLeft].SetLoopFlag(true);
+	//‰E•à‚«
+	animClip[enWalkRight].Load("Assets/animData/DragonNightmare/Nm_walkright.tka");
+	animClip[enWalkRight].SetLoopFlag(true);
+	//‘–‚è
+	animClip[enRun].Load("Assets/animData/DragonNightmare/Nm_run.tka");
+	animClip[enRun].SetLoopFlag(true);
+	//™ôšK
+	animClip[enScream].Load("Assets/animData/DragonNightmare/Nm_scream.tka");
+	//UŒ‚
+	animClip[enBasicAttack].Load("Assets/animData/DragonNightmare/Nm_basicattack.tka");
+	//’ÜUŒ‚
+	animClip[enClawAttack].Load("Assets/animData/DragonNightmare/Nm_clawattack.tka");
+	//ŠpUŒ‚
+	animClip[enHornAttack].Load("Assets/animData/DragonNightmare/Nm_hornattack.tka");
+	//–hŒä
+	animClip[enDefend].Load("Assets/animData/DragonNightmare/Nm_defend.tka");
+	//–hŒä
+	animClip[enDefend].Load("Assets/animData/DragonNightmare/Nm_sleep.tka");
+	//”íƒ_ƒ
+	animClip[enGethit].Load("Assets/animData/DragonNightmare/Nm_gethit.tka");
+	//€–S
+	animClip[enDie].Load("Assets/animData/DragonNightmare/Nm_die.tka");
+	return true;
+}
+
+void NightmareAnimation::Update()
+{
+
+}
--- sotukenGame/Sample_15_05/Sample_15_05/NightmareAnimation.h
+++ sotukenGame/Sample_15_05/Sample_15_05/NightmareAnimation.h
@@ -1,7 +1,44 @@
 #pragma once
-class NightmareAnimation
+const enum NightmAnim {
+	enIdle01,                   //待機01
+	enIdle02,                   //待機02
+	enWalk,                    //歩き
+	enWalkBack,                //後ろ歩き
+	enWalkLeft,                //左歩き
+	enWalkRight,               //右歩き
+	enRun,                     //走り
+	enScream,                  //咆哮
+	enBasicAttack,             //攻撃
+	enClawAttack,              //爪攻撃
+	enHornAttack,              //角攻撃
+	enDefend,                  //防御
+	enSleep,                   //睡眠
+	enGethit,                  //被ダメ
+	enDie,                     //死亡
+	enNightmAnimClip_num       //アニメーションの数
+};
+class NightmareAnimation : public IGameObject
 {
 	NightmareAnimation();
 	~NightmareAnimation();
+	/// <summary>
+	/// スタート関数。
+	/// </summary>
+	/// <returns></returns>
+	bool Start() override;
+	/// <summary>
+	/// 更新関数。
+	/// </summary>
+	void Update() override;
+	/// <summary>
+	/// アニメーションクリップを返す。
+	/// </summary>
+	/// <returns></returns>
+	AnimationClip* GetAnimationClip()
+	{
+		return animClip;
+	}
+private:
+	AnimationClip animClip[enNightmAnimClip_num];//アニメーションクリップ。
 };
 
