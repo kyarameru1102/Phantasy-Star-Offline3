7e8cc98261440975060567d033f4c4d1041bd93e
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2021-02-02 13:33:01 +0900

    攻撃パターンを変更

--- sotukenGame/Sample_15_05/Sample_15_05/DrTerrorBringer.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/DrTerrorBringer.cpp
@@ -47,6 +47,8 @@ bool DrTerrorBringer::Start()
 
 void DrTerrorBringer::Move()
 {
+	Vector3 playerLen = m_toPlayer;
+	playerLen.Normalize();
 	if (m_FlyFlag == true)
 	{
 		m_status = Fly_state;
@@ -54,6 +56,7 @@ void DrTerrorBringer::Move()
 	if (m_SetFly == true)
 	{
 		m_status = FlyMove_state;
+		m_movespeed = playerLen * -2.0;
 	}
 	if (m_LandFlag == true)
 	{
@@ -62,12 +65,11 @@ void DrTerrorBringer::Move()
 	else if(m_FlyFlag == false && m_SetFly == false && m_LandFlag == false)
 	{
 		m_status = Run_state;
+		m_movespeed = playerLen * 1.4f;
+		m_movespeed.y = m_speedY;
 	}
 
-	Vector3 playerLen = m_toPlayer;
-	playerLen.Normalize();
-	m_movespeed = playerLen * 1.2f;
-	m_movespeed.y = m_speedY;
+	
 	m_position = m_charaCon.Execute(1.0f, m_movespeed);
 }
 void DrTerrorBringer::Turn()
@@ -175,7 +177,11 @@ void DrTerrorBringer::Update()
 
 	//プレイヤーに近づく。
 	if (m_status != GetDamage_state) {
-		Scream();
+		
+		if (m_screamflag == true)
+		{
+			Scream();
+		}
 		if (m_screamflag ==false && m_status != Attack_state && m_status != WingClawAttack_state && m_status != FlameAttack_state && m_status != Die_state) {
 			
 			Move();
@@ -183,7 +189,8 @@ void DrTerrorBringer::Update()
 		}
 
 		//距離が近づくと。
-		if (m_screamflag == false)
+		//パターン１
+		if (m_screamflag == false && m_ATKOne == true)
 		{
 			//噛みつき攻撃
 			if (m_isFangATK == true)
@@ -212,8 +219,36 @@ void DrTerrorBringer::Update()
 			}
 			if (m_FlameATKCount == 1)
 			{
-				m_isFangATK = true;
+				m_screamflag = true;
 				m_FlameATKCount = 0;
+				m_ATKTwe = true;
+				m_ATKOne = false;
+			}
+		}
+		//パターン２
+		if (m_screamflag == false && m_ATKTwe == true)
+		{
+			//後ろに飛ぶ
+			if (m_TweStart == false)
+			{
+				m_FlyFlag = true;
+			}
+			
+
+			//火炎攻撃をする
+			if (m_TweStart == true && m_FlyFlag == false && m_SetFly == false && m_LandFlag == false)
+			{
+				m_isFlameATK = true;
+			}
+			
+			if (m_isFlameATK == true)
+			{
+				FlameAttack();
+			}
+			else
+			{
+				m_ATKOne = true;
+				m_ATKTwe = false;
 			}
 		}
 		
@@ -254,6 +289,7 @@ void DrTerrorBringer::Update()
 		if (!m_skinModelRender->GetisAnimationPlaing())
 		{
 			m_LandFlag = false;
+			m_TweStart = true;
 			m_animState = TerrorBringerAnimInfo::enTe_Idle01;
 			m_skinModelRender->PlayAnimation(m_animState, 0.0f);
 		}
